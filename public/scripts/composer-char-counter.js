69e506734e756abdb451b21d1ed0f944d0a4e67b
Lindsay Stewart <lj.stewart00@gmail.com>
2018-10-12 00:57:18 +0000

    added compose button

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('.new-tweet-text').on('keydown', function() {
+$(document).ready(function () {
+  $('.new-tweet-text').on('keydown', function () {
     let remainingCharacters = 140 - $(this).val().length;
     console.log(remainingCharacters);
     $('#counter').text(remainingCharacters);
