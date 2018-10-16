6dadd47c4c9f6a7736e62b62f89fd00362bbec9c
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-15 22:00:32 -0700

    changed time stamp to valid date

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
-$(function () {
 
+$(function () {
+  // building createTweetElement
   function createTweetElement(tweetData) {
     var $article = $('<article>').addClass('tweet-container');
     var $header = $('<header>').addClass('tweet-container-header');
@@ -13,11 +14,15 @@ $(function () {
       .addClass('twitterhandle')
       .text(tweetData.user.handle);
     var $section = $('<section>').text(tweetData.content.text);
-    // var $p = $('<p>')
-    var $footer = $('<footer>').text(tweetData.created_at);
-
+    var $footer = $('<footer>')
+    var $icons = $('<div>').addClass('icons');
+    var $timestamp = $('<div>').text(moment(new Date(tweetData.created_at)).fromNow())
+    // inserted favicons
+    var $heartIcon = $('<i class = "fa fa-heart" >');
+    var $retweetIcon = $('<i class = "fa fa-retweet" >');
+    var $flagIcon = $('<i class = "fa fa-flag">');
+    // appending children to parent elements
     $section = $section
-    // .append($p);
     $header = $header
       .append($image)
       .append($h2)
@@ -26,13 +31,18 @@ $(function () {
       .append($header)
       .append($section)
       .append($footer)
-    let $tweet = $article;
+    $icons
+      .append($flagIcon)
+      .append($retweetIcon)
+      .append($heartIcon);
+    $footer
+      .append($timestamp)
+      .append($icons);
 
-    // $tweet.append(tweetData);
+    let $tweet = $article;
+    // return result
     return $tweet.append(tweetData);
   }
-  //let $tweet = createTweetElement(tweetData);
-  //console.log($tweet);
 
   const data = [
     {
@@ -85,45 +95,51 @@ $(function () {
   function renderTweets(tweets) {
     // loops through tweets
     tweets.forEach(function (element) {
+      // attach tweets to top of tweet container
       var result = createTweetElement(element);
       $('.alltweetscontainer').prepend(result);
-      console.log(element);
     });
   }
-
   renderTweets(data);
 
+  //event handler for submission of new tweets
   var $newTweetForm = $('#new-tweet').on('submit', function (event) {
     // prevent submitting and reloading page
     event.preventDefault();
     let tweetLength = $('#new-tweet-text').val().length;
+    // default hide error message
     $(".error-message").slideUp();
+    // if invalid tweet upon submit show errors
     if (tweetLength <= 0) {
       $(".error-message").text("you gotta write something first!").slideDown()
     } else if (tweetLength > 140) {
       $(".error-message").text("still too long...").slideDown()
     } else {
       console.log('submit successful');
+      // show tweet as text string
       var data = $newTweetForm.serialize();
+      // resets text box and counter upon submission
       $('#new-tweet-text').val("");
+      $('#counter').text('140');
     }
 
+    // POST tweets using ajax
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: data,
       success: function (result) {
+        // attach newest tweet to top
         $('.alltweetscontainer').prepend(data);
         loadTweets();
       },
       error: function (err) { }
     });
   });
-  console.log($newTweetForm);
 
   function loadTweets() {
+    // GET tweets
     $.get('/tweets', function (tweets) {
-      $("#tweets").empty();
       renderTweets(tweets);
     });
   }
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,25 @@
 $(function () {
+  // event handler for new tweet text input
   $('#new-tweet-text').on('input', function () {
+    // subtract tweet length from character limit
     let remainingCharacters = 140 - $(this).val().length;
     $('#counter').text(remainingCharacters);
+    // change character counter color to red if limit exceeded, otherwise black
     if (remainingCharacters < 0) {
       $('#counter').css('color', 'red');
     } else {
       $('#counter').css('color', 'black');
     }
+    // error messages upon invalid tweets input, otherwise
     let tweetLength = $('#new-tweet-text').val().length;
     if (tweetLength <= 0) {
       $(".error-message").text("you gotta write something first!").slideDown()
     } else if (tweetLength > 140) {
       $(".error-message").text("that's a bit too long.").slideDown()
     } else {
+      // otherwise hide error message
       $(".error-message").slideUp();
     }
   });
 });
 
-// $("#counter").addClass("counter-negative");
-// $("#counter").removeClass
